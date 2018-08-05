d2e7c633c5db380febd00107ebe84b247733c82a
Corentin Ferry <corentin.ferry@ens-rennes.fr>
2018-08-05 11:10:03 +0000

    Update the base read address only once the buffer has been entirely read

--- fpga/MIC_ARRAY/hw/hdl/Output_Buffer_Driver.vhd
+++ fpga/MIC_ARRAY/hw/hdl/Output_Buffer_Driver.vhd
@@ -80,6 +80,7 @@ architecture master of Output_Buffer_Driver is
 
     -- Configuration Slave (constants for now)
     signal base_read_addr : unsigned(31 downto 0) := to_unsigned(900 * 1024 * 1024, 32);  -- 900 MiB = 900 * 1024 KiB = 900 * 1024 * 1024 B
+    signal base_read_new  : unsigned(31 downto 0) := to_unsigned(900 * 1024 * 1024, 32);  -- 900 MiB = 900 * 1024 KiB = 900 * 1024 * 1024 B
     signal sound_len      : unsigned(31 downto 0) := to_unsigned(100 * 1024 * 1024, 32);  -- 100 MiB
     constant byteEnable   : unsigned(3 downto 0)  := "1111";  -- 32 bits are enabled ( L + R )
     signal Use_Memory     : std_logic             := '1';
@@ -103,6 +104,8 @@ begin
             DMA_Read          <= '0';
             signal_holder_DMA <= (others => '0');
             DataOK_DMA        <= '0';
+            base_read_addr    <= to_unsigned(900 * 1024 * 1024, 32);
+            SndAddr           <= to_unsigned(900 * 1024 * 1024, 32);
         elsif rising_edge(clk) then
             if Enable = '1' then
                 if Use_Memory = '1' then
@@ -148,7 +151,9 @@ begin
                                         Buffer2 <= '1';
                                     end if;
                                 else
-                                    SndAddr <= base_read_addr;
+                                    -- Update the base address only here. 
+                                    SndAddr <= base_read_new;
+                                    base_read_addr <= base_read_new;
                                     Buffer1 <= '1';
                                     Buffer2 <= '0';
                                 end if;
@@ -167,14 +172,16 @@ begin
                     DMA_ByteEnable <= (others => '0');
                     DMA_Read       <= '0';
                     DataOK_DMA     <= '0';
-                    SndAddr        <= base_read_addr;
+                    SndAddr        <= base_read_new;
+                    base_read_addr <= base_read_new;
                 end if;
             else
                 DMA_Addr       <= (others => '0');
                 DMA_ByteEnable <= (others => '0');
                 DMA_Read       <= '0';
                 DataOK_DMA     <= '0';
-                SndAddr        <= base_read_addr;
+                SndAddr        <= base_read_new;
+                base_read_addr <= base_read_new;
             end if;
         end if;
     end process;
@@ -310,7 +317,7 @@ begin
     configSlave : process(reset_n, clk)
     begin
         if reset_n = '0' then
-            base_read_addr <= to_unsigned(900 * 1024 * 1024, 32);
+            base_read_new  <= to_unsigned(900 * 1024 * 1024, 32);
             sound_len      <= to_unsigned(100 * 1024 * 1024, 32);
             Use_Memory     <= '0';  -- by default we use the Avalon Streaming (to avoid hearing grabage)
             Enable         <= '0';
@@ -318,7 +325,7 @@ begin
             if Cfg_Avalon_Write = '1' then
                 case Cfg_Avalon_Address is
                     when "000" =>
-                        base_read_addr(31 downto 0) <= unsigned(Cfg_Avalon_WriteData);
+                        base_read_new(31 downto 0) <= unsigned(Cfg_Avalon_WriteData);
                     when "001" =>
                         sound_len(31 downto 0) <= unsigned(Cfg_Avalon_WriteData);
                     when "010" =>
@@ -330,7 +337,8 @@ begin
             elsif Cfg_Avalon_Read = '1' then
                 Cfg_Avalon_ReadData(31 downto 0) <= (others => '0');
                 case Cfg_Avalon_Address is
-                    when "000" =>
+                    when "000" => -- Old value of the register : the one that is still under readback.
+                        -- The value of this register is updated once the new buffer is being read.
                         Cfg_Avalon_ReadData(31 downto 0) <= std_logic_vector(base_read_addr(31 downto 0));
                     when "001" =>
                         Cfg_Avalon_ReadData(31 downto 0) <= std_logic_vector(sound_len(31 downto 0));
